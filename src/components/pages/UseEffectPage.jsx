f740d4c58193ebd4758f42ea8cc153885e3a4b89
enguidev <carlosengui10@gmail.com>
2025-12-14 20:34:12 +0100

    BtnVolver

--- src/components/pages/UseEffectPage.jsx
+++ src/components/pages/UseEffectPage.jsx
@@ -1,6 +1,8 @@
+import { BtnVolver } from "../components/ui/BtnVolver";
 export const UseEffectPage = () => {
   return (
     <div className="h-screen bg-amber-300 text-black">
+      <BtnVolver />
       <span>UseEffectPage</span>
     </div>
   );
