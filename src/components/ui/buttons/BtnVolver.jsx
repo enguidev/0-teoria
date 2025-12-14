6615a9e34fef613fb7497f3bd379a8ddb6602c81
enguidev <carlosengui10@gmail.com>
2025-12-14 22:08:54 +0100

    cambiado div de BtnVolver a Button

--- src/components/ui/buttons/BtnVolver.jsx
+++ src/components/ui/buttons/BtnVolver.jsx
@@ -1,8 +1,8 @@
 import { Icon } from "@iconify/react/dist/iconify";
 export const BtnVolver = () => {
   return (
-    <div className="absolute top-4 left-4 bg-white text-black hover:bg-[#f2f2f2] p-3 rounded-full shadow-lg cursor-pointer">
+    <button className="absolute top-4 left-4 bg-white text-black hover:bg-[#f2f2f2] p-3 rounded-full shadow-lg cursor-pointer">
       <Icon icon="noto-v1:left-arrow" width="24" height="24" />
-    </div>
+    </button>
   );
 };
