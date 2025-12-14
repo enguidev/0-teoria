7e5310aa75d3eb76c332d40181b7930ba381ef1d
enguidev <carlosengui10@gmail.com>
2025-12-14 22:20:07 +0100

    implementado onClick de useNavigate

--- src/components/ui/buttons/BtnVolver.jsx
+++ src/components/ui/buttons/BtnVolver.jsx
@@ -1,7 +1,9 @@
 import { Icon } from "@iconify/react/dist/iconify";
+import { useNavigate } from "react-router-dom";
 export const BtnVolver = () => {
+  const navigate = useNavigate();
   return (
-    <button className="absolute top-4 left-4 bg-white text-black hover:bg-[#f2f2f2] p-3 rounded-full shadow-lg cursor-pointer">
+    <button onClick={()=>navigate(-1)} className="absolute top-4 left-4 bg-white text-black hover:bg-[#f2f2f2] p-3 rounded-full shadow-lg cursor-pointer">
       <Icon icon="noto-v1:left-arrow" width="24" height="24" />
     </button>
   );
