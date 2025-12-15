c8494082b891e36d6aa8d29c9e92fb878f686812
enguidev <carlosengui10@gmail.com>
2025-12-15 01:00:28 +0100

    Useeffect contador de segundos

--- src/components/pages/UseEffectPage.jsx
+++ src/components/pages/UseEffectPage.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { BtnVolver } from "../ui/buttons/btnVolver";
 
 export const UseEffectPage = () => {
+  const [segundos, setSegundos] = useState(0);
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setSegundos((s) => s + 1);
+    }, 1000);
+    return () => clearInterval(intervalo);
+  }, []);
   return (
     <div className="h-screen bg-amber-300 text-black">
       <BtnVolver />
-      <span>UseEffectPage</span>
+      <h2>UseEffectPage</h2>
+      <span>{segundos}</span>
     </div>
   );
 };
